a97b02ce1ad454aa6d92e8f1a293b50804420687
Mahmoud Ayman <mahmoud.ayman.fcai@gmail.com>
2025-01-01 21:26:43 +0200

    First test

--- tower_of_hanoi.cpp
+++ tower_of_hanoi.cpp
@@ -13,3 +13,26 @@ void tower_of_hanoi(int n, char from, char aux, char to)
     cout << "Move disk " << n << " from rod " << from << " to rod " << to << endl;
     tower_of_hanoi(n-1, aux, from, to);
 }
+
+
+void tower_of_hanoi2(int n, char from, char aux, char to)
+{
+    if (n == 1)
+    {
+        cout << "Move disk " << n << " from rod " << from << " to rod " << to << endl;
+    }
+    else
+    {
+        tower_of_hanoi(n-1, from, to, aux);
+        cout << "Move disk " << n << " from rod " << from << " to rod " << to << endl;
+        tower_of_hanoi(n-1, aux, from, to);
+    }
+}
+
+
+
+int main()
+{
+    int n = 2;
+    tower_of_hanoi2(n, 'A', 'B', 'C');
+}
